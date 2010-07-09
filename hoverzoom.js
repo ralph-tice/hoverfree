a04ccc0f6d8219b15153bbea71fd3bc93654a014
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-09 17:05:57 +0000

    Hoverzoom first import

---
+++ hoverzoom.js
@@ -0,0 +1,92 @@
+﻿function hoverZoom() {
+
+	var wnd = $(window);
+	var doc = $(document);
+
+	// Div containing the full size image
+	var hoverZoomImg = $('<div id="hoverZoomImg"></div>');
+	hoverZoomImg.appendTo('body');
+	
+	var imgLoading = $('<img>').attr('src', chrome.extension.getURL('loading.gif'));
+	
+	var lastSrc = '';
+	var mousePos;
+	var loading = false;
+	
+	function posImg(position) {
+		position.top -= hoverZoomImg.height() / 2;
+		if (!loading && hoverZoomImg.height() > wnd.height())
+			$('#hoverZoomImg img').height(wnd.height() - 10);
+		if (position.top + hoverZoomImg.height() >= wnd.scrollTop() + wnd.height() - 10)
+			position.top = wnd.scrollTop() + wnd.height() - hoverZoomImg.height() - 15;
+		if (position.top < wnd.scrollTop())
+			position.top = wnd.scrollTop();
+		if (position.left + hoverZoomImg.width() > wnd.scrollLeft() + wnd.width() && position.left > wnd.scrollLeft() + wnd.width() / 2) {
+			position.left -= hoverZoomImg.width() + 20;
+		} else {
+			position.left += 10;
+		}
+		hoverZoomImg.offset(position);
+	}
+
+	var documentMouseMove = function(event) {	
+		mousePos = {top:event.pageY, left:event.pageX};
+		posImg(mousePos);
+	};
+	
+	function getImgLinks() {
+		return $("a[href]").filter(function(index) {
+			return this.href.match(/\.(jpg|jpeg|gif|png)$/i);
+		});
+	}
+	
+	// Mouse enters link
+	function imgLinkMouseEnter(event) {
+		posImg({top:event.pageY, left:event.pageX});
+		doc.bind('mousemove', documentMouseMove);
+		
+		// Loading image
+		loading = true;
+		imgLoading.appendTo(hoverZoomImg);
+		hoverZoomImg.show();
+		
+		// Full size image
+		lastSrc = $(this).attr('href');
+		$('<img>').attr('src', $(this).attr('href')).load(function () {
+			if (lastSrc == $(this).attr('src')) {
+				loading = false;
+				hoverZoomImg.empty();
+				$(this).appendTo(hoverZoomImg);
+				posImg(mousePos);					
+			}
+		});
+	}
+	
+	// Mouse leaves link 
+	function imgLinkMouseLeave(event) {
+		doc.unbind('mousemove', documentMouseMove);
+		hoverZoomImg.hide();
+		hoverZoomImg.empty();
+	}
+	
+	function bindImgLinks() {
+	
+		// Links to images
+		var imgLinks = getImgLinks();
+
+		imgLinks.unbind('mouseenter', imgLinkMouseEnter);
+		imgLinks.unbind('mouseleave', imgLinkMouseLeave);
+		imgLinks.bind('mouseenter', imgLinkMouseEnter);
+		imgLinks.bind('mouseleave', imgLinkMouseLeave);
+	}
+	
+	bindImgLinks();
+	
+	$(window).bind('DOMNodeInserted', function(event) {
+		//console.log(event.srcElement);
+		if (event.srcElement && $(event.srcElement).find('a[href]').length)
+			bindImgLinks();
+	});
+};
+
+hoverZoom();
