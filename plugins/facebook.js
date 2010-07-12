4301ecf1a3b783f24d7c8d7f4a227eaea7044d2f
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-12 16:51:05 +0000

    Facebook plugin improvements

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -3,13 +3,23 @@ hoverZoomPlugins.push( {
 	"name": "Facebook",
 	"version": "1.0",
 	"prepareImgLinks": function() {
-		var links = $("a.uiPhotoThumb,a.UIImageBlock_MED_Image,a.UIImageBlock_SMALL_Image");
+		var links = $("a.uiPhotoThumb,a.UIImageBlock_MED_Image,a.UIImageBlock_SMALL_Image,td.UIFullListing_Pic a,li.buddyRow a,a.chat_info_pic_link,a.UIPhotoGrid_PhotoLink");
 		links.each(function(index, el) {
+			
+			// Thumbnail URL
 			var src = $(el).find('img')[0].src;
-			if (src.indexOf('safe_image.php') > -1) {
-				src = unescape(src.substr(src.indexOf('&url=') + 5));				
+			
+			// If image URL is included as a querystring parameter
+			var indexQS = src.indexOf('&url=');
+			if (indexQS == -1)
+				indexQS = src.indexOf('&src=');
+				
+			if (indexQS > -1) {
+				src = unescape(src.substr(indexQS + 5));
+				if (src.indexOf('&') > -1)
+					src = src.substr(0, src.indexOf('&'));
 			} else {
-				src = src.replace(/photos-\w/, 'sphotos').replace(/_[sq]\./, '_n.').replace(/\/q/, '/n');
+				src = src.replace(/photos-\w/, 'sphotos').replace(/_[sqta]\./, '_n.').replace(/\/q/, '/n');
 			}
 			$(el).data('hoverzoomsrc', src);
 		});
