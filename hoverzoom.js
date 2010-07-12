c3396138d518dec053dea7dfa2a81e670c5245e2
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-12 16:54:34 +0000

    Changed the way mouse events are handled. Image width is now adjusted too.

--- hoverzoom.js
+++ hoverzoom.js
@@ -2,87 +2,113 @@
 function hoverZoom() {
 
 	var wnd = $(window);
-	var doc = $(document);
 
-	// Div containing the full size image
 	var hoverZoomImg = $('<div id="hoverZoomImg"></div>');
 	hoverZoomImg.appendTo('body');
+	var imgFullSize = null;		
+	var imgLoading = $('<img />').attr('src', chrome.extension.getURL('loading.gif'));
 	
-	var imgLoading = $('<img>').attr('src', chrome.extension.getURL('loading.gif'));
-	
-	var lastSrc = '';
-	var mousePos;
+	var imgSrc = '';
+	var mousePos = {};
 	var loading = false;
 	
 	function posImg(position) {
-		position.top -= hoverZoomImg.height() / 2;
-		if (!loading && hoverZoomImg.height() > wnd.height())
-			$('#hoverZoomImg img').height(wnd.height() - 10);
-		if (position.top + hoverZoomImg.height() >= wnd.scrollTop() + wnd.height() - 10)
-			position.top = wnd.scrollTop() + wnd.height() - hoverZoomImg.height() - 15;
-		if (position.top < wnd.scrollTop())
-			position.top = wnd.scrollTop();
-		if (position.left + hoverZoomImg.width() > wnd.scrollLeft() + wnd.width() && position.left > wnd.scrollLeft() + wnd.width() / 2) {
-			position.left -= hoverZoomImg.width() + 20;			
+		if (!imgFullSize)
+			return;
+		position = position || mousePos;
+		
+		var offset = 20;
+		
+		if (loading) {
+			position.left += offset;
+			position.top -= 10;
 		} else {
-			position.left += 10;
+			var margin = 8;
+			var padding = 6;
+			
+			imgFullSize.width('auto').height('auto');
+			var naturalWidth = imgFullSize.width();
+			var naturalHeight = imgFullSize.height();
+			if (!naturalWidth || !naturalHeight)
+				return;
+			
+			var displayOnRight = true;
+			if (position.left - wnd.scrollLeft() < wnd.width() / 2) {
+				position.left += offset;
+				if (naturalWidth + padding > wnd.width() - position.left) {
+					imgFullSize.width(wnd.width() - position.left - padding + wnd.scrollLeft());
+				}
+			} else {
+				displayOnRight = false;
+				position.left -= offset;
+				if (naturalWidth + padding > position.left) {
+					imgFullSize.width(position.left - padding - wnd.scrollLeft());
+				}			
+			}
+			position.top -= hoverZoomImg.height() / 2;
+			if (hoverZoomImg.height() > wnd.height())
+				imgFullSize.height(wnd.height() - padding).width('auto');
+			if (!displayOnRight) 
+				position.left -= hoverZoomImg.width() + padding;
+			if (position.top + hoverZoomImg.height() >= wnd.scrollTop() + wnd.height())
+				position.top = wnd.scrollTop() + wnd.height() - hoverZoomImg.height() - padding;
+			if (position.top < wnd.scrollTop())
+				position.top = wnd.scrollTop();	
 		}
+		
 		hoverZoomImg.offset(position);
 	}
 
-	var documentMouseMove = function(event) {	
+	function documentMouseMove(event) {
 		mousePos = {top:event.pageY, left:event.pageX};
-		posImg(mousePos);
-	};
-	
-	// Mouse enters link
-	function imgLinkMouseEnter(event) {
-		//if ($(this).data('hoverzoomsrc') != '')
-			//return;
-			
-		posImg({top:event.pageY, left:event.pageX});
-		doc.bind('mousemove', documentMouseMove);
+		var links = $(event.target).parents('.hoverZoomLink');
+		if (links.length > 0) {
 		
-		// Loading image
-		loading = true;
-		imgLoading.appendTo(hoverZoomImg);
-		hoverZoomImg.show();
-		
-		// Full size image
-		lastSrc = $(this).data('hoverzoomsrc');
-		$('<img>').attr('src', $(this).data('hoverzoomsrc')).load(function () {
-			if (lastSrc == $(this).attr('src')) {
-				loading = false;
+			// Happens when the mouse goes from an image to another without hovering the page background
+			if (links.data('hoverzoomsrc') != imgSrc) {
 				hoverZoomImg.empty();
-				$(this).appendTo(hoverZoomImg);
-				posImg(mousePos);	
-console.log(lastSrc);				
+				imgFullSize = null;
 			}
-		});
-	}
-	
-	// Mouse leaves link 
-	function imgLinkMouseLeave(event) {
-		doc.unbind('mousemove', documentMouseMove);
-		hoverZoomImg.hide();
-		hoverZoomImg.empty();
+			
+			imgSrc = links.data('hoverzoomsrc');
+			
+			// If no image is currently displayed...
+			if (!imgFullSize) {
+				loading = true;
+				imgLoading.appendTo(hoverZoomImg);
+				hoverZoomImg.show();
+				imgFullSize = $('<img />').attr('src', imgSrc).load(function () {				
+					// Only the last hovered link gets displayed
+					if (imgSrc == $(this).attr('src')) {
+						loading = false;
+						hoverZoomImg.offset({top:-9000, left:-9000}); 	// hides the image while making it available for size calculations
+						hoverZoomImg.empty();
+						$(this).appendTo(hoverZoomImg);
+						posImg();
+					}
+				});
+			}
+			posImg();
+		} else {
+			imgFullSize = null;
+			hoverZoomImg.empty();
+			hoverZoomImg.hide();
+		}
 	}
 	
 	function bindImgLinks() {
 		for (i in hoverZoomPlugins) {
-			var imgLinks = hoverZoomPlugins[i].prepareImgLinks();
-			imgLinks.unbind('mouseenter', imgLinkMouseEnter);
-			imgLinks.unbind('mouseleave', imgLinkMouseLeave);
-			imgLinks.unbind('mousedown', imgLinkMouseLeave);
-			imgLinks.bind('mouseenter', imgLinkMouseEnter);
-			imgLinks.bind('mouseleave', imgLinkMouseLeave);
-			imgLinks.bind('mousedown', imgLinkMouseLeave);
+			hoverZoomPlugins[i].prepareImgLinks().each(function() {
+				$(this).addClass('hoverZoomLink');
+			});
 		}
 	}
 	
 	bindImgLinks();
 	
-	$(window).bind('DOMNodeInserted', function(event) {
+	$(document).bind('mousemove', documentMouseMove);
+		
+	wnd.bind('DOMNodeInserted', function(event) {
 		if (event.srcElement && (
 		   (event.srcElement.nodeName && event.srcElement.nodeName.toLowerCase() == 'a') ||
 		   $(event.srcElement).find('a[href]').length))
