9c6ef7ebb632c5b533981664495a70d0f147d61b
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-13 10:07:48 +0000

    Default plugin now excludes URLs with a query string

--- plugins/default.js
+++ plugins/default.js
@@ -1,10 +1,10 @@
-var hoverZoomPlugins = hoverZoomPlugins ? hoverZoomPlugins : [];
+var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	"name": "Default",
 	"version": "0.1",
 	"prepareImgLinks": function() {
 		var links = $("a[href]").filter(function(index) {
-			return this.href.match(/\.(jpg|jpeg|gif|png)$/i);
+			return this.href.match(/^[^\?]*\.(jpg|jpeg|gif|png)$/i);
 		});
 		links.each(function() {
 			$(this).data('hoverzoomsrc', this.href);
