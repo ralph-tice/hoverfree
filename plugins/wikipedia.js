8059941f1f8d1671781426543cf22dae94284bed
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-15 07:49:09 +0000

    Added Wikipedia plugin

---
+++ plugins/wikipedia.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	"name": "Wikipedia",
+	"version": "0.1",
+	"prepareImgLinks": function() {
+		var links = $("a.image, div.l_image a");
+		links.each(function() {		
+			var src = $(this).find('img')[0].src;
+			var ext = src.substr(src.lastIndexOf('.'));
+			src = src.substring(0, src.indexOf(ext) + 4).replace(/thumb\//, '');
+			$(this).data('hoverzoomsrc', src);
+		});
+		return links;		
+	}
+});
