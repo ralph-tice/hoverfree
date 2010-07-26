bcac32d5c077b959416147d2e7dd17b5bb3e4655
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-26 20:03:20 +0000

    Added MySpace plugin

---
+++ plugins/myspace.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'MySpace',
+	version: '0.1',
+	prepareImgLinks: function() {
+		var imgs = $('a img[src*=ac-images.myspacecdn.com], a img[src*=images.socialplan.com]');
+		var res = [];
+		imgs.each(function() {
+			var link = $(this).parents('a:eq(0)');
+			var src = $(this).attr('src');
+			src = src.replace(/\/[sm]_/, '/l_').replace('_t.', '_p.');
+			link.data('hoverZoomSrc', [src]);
+			res.push(link);
+		});
+		return $(res);	
+	}
+});
