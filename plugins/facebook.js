ebc95b5baa1d95b2ab7db84be9c168bcedcc3d39
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-27 13:11:17 +0000

    Added support for Facebook photo album covers

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -3,19 +3,22 @@
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
-	"name": "Facebook",
-	"version": "0.4",
-	"prepareImgLinks": function() {
-		var links = $();
-		var imgs = $('a img.img:not([src^="http://static.ak.fbcdn.net"]),a img.UIProfileImage'); //:not(.loader):not(.throbber)
-		imgs.each(function() {
-			links = links.add($(this).parents('a').get(0));
-		});
-		links.each(function() {
+	name: 'Facebook',
+	version: '0.5',
+	prepareImgLinks: function() {
+	
+		function srcReplace(src) {
+			return src.replace(/photos-\w/, 'sphotos').replace(/_[sqta]\./, '_n.').replace(/\/[sqta](\d)/, '/n$1');
+		}
+	
+		var res = [];
+		
+		$('a img.img:not([src^="http://static.ak.fbcdn.net"]),a img.UIProfileImage').each(function() {			
+			var _this = $(this);
+			var link = _this.parents('a:eq(0)');
 			
 			// Thumbnail URL
-			var img = $(this).find('img').get(0);
-			var src = unescape(img.src);
+			var src = unescape(_this.attr('src'));
 			
 			// If image URL is included as a querystring parameter
 			var indexQS = src.lastIndexOf('&url=');
@@ -27,16 +30,30 @@ hoverZoomPlugins.push( {
 				if (src.indexOf('&') > -1)
 					src = src.substr(0, src.indexOf('&'));
 			} else {
-				src = src.replace(/photos-\w/, 'sphotos').replace(/_[sqta]\./, '_n.').replace(/\/[sqta](\d)/, '/n$1');
+				src = srcReplace(src);
 			}
 			
-			$(this).data('hoverZoomSrc', [src]);
+			link.data('hoverZoomSrc', [src]);
 			
-			var tooltip = $(this).find('.uiTooltipText:eq(0)');
+			var tooltip = _this.find('.uiTooltipText:eq(0)');
 			if (tooltip.length) {
-				$(this).data('hoverZoomCaption', tooltip.text());
+				link.data('hoverZoomCaption', tooltip.text());
 			}
+			
+			res.push(link);
 		});
-		return links;		
+		
+		// Photo albums covers
+		$('.album_thumb a.album_link').each(function() {
+			var _this = $(this);
+			var div = _this.parent();
+			var src = div.attr('style');
+			src = src.substring(src.indexOf('http:'), src.lastIndexOf(')'));
+			src = srcReplace(src);
+			_this.data('hoverZoomSrc', [src]);
+			res.push(_this);
+		});
+		
+		return $(res);		
 	}
 });
