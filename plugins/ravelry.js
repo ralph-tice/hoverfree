df40e140b851c1e74fed0af835e9ea4913244016
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-09-27 20:12:59 +0000

    Added backpage.com plugin.

--- plugins/ravelry.js
+++ plugins/ravelry.js
@@ -7,17 +7,6 @@ hoverZoomPlugins.push( {
 	version: '0.1',
 	prepareImgLinks: function(callback) {
 		var res = [];
-		
-		/*$('.thumbnail, .zoomable_photo, .photo').each(function() {
-			var _this = $(this);
-			var src = _this.attr('style');
-			if (!src) return;
-			src = src.substr(src.indexOf('http:'));
-			src = src.substr(0, src.indexOf("'"));
-			src = src.replace('_small', '').replace('_best_fit', '').replace(/_[mst]./, '.');
-			_this.data('hoverZoomSrc', [src]);
-			res.push(_this);
-		});*/
 
 		hoverZoom.urlReplace(res, 
 			'.thumbnail, .zoomable_photo, .photo',
