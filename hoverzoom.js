0d6e41552f7a6054b3d96150f7f68ad6c976705d
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-09-29 14:53:23 +0000

    Added experimental "display in a window" feature (disabled). Fixed imgur plugin.

--- hoverzoom.js
+++ hoverzoom.js
@@ -19,7 +19,8 @@ var hoverZoom = {
 			options = {},
 			actionKeyDown = false,
 			fullZoomKeyDown = false,
-			pageActionShown = false;
+			pageActionShown = false,
+			hoverZoomWin = null;
 		
 		// Calculate optimal image position and size
 		function posImg(position) {
@@ -120,6 +121,16 @@ var hoverZoom = {
 			
 			position = {top: Math.round(position.top), left: Math.round(position.left)};
 			hoverZoomImg.css(position);
+			
+			/*if (hoverZoomWin) {
+				hoverZoomImg.css({top: -9999, left: -9999, height: 'auto', width: 'auto'});
+				chrome.extension.sendRequest({action : 'updateWindow', windowId: hoverZoomWin.id, updateInfo: {
+					top: position.top,
+					left: window.screenLeft + position.left,
+					width: imgFullSize.width(),
+					height: imgFullSize.height()
+				}});
+			}*/
 		}
 		
 		function hideHoverZoomImg(now) {
@@ -127,6 +138,10 @@ var hoverZoom = {
 				return;
 			}
 			imgFullSize = null;
+			/*if (hoverZoomWin) {
+				chrome.extension.sendRequest({action : 'removeWindow', windowId: hoverZoomWin.id });
+				hoverZoomWin = null; 
+			}*/
 			if (loading) { now = true; }
 			hoverZoomImg.stop(true, true).fadeOut(now ? 0 : options.fadeDuration, function() {
 				hoverZoomCaption = null;
@@ -219,6 +234,18 @@ var hoverZoom = {
 						if (options.addToHistory && !chrome.extension.inIncognitoTab) {
 							chrome.extension.sendRequest({action : 'addUrlToHistory', url: imgSrc});
 						}
+						
+						/*chrome.extension.sendRequest({action : 'createWindow', createData: {
+							url: imgSrc, 
+							type: 'popup',
+							top: 0,
+							left: 0,
+							width: imgFullSize.width(),
+							height: imgFullSize.height(),
+							incognito: chrome.extension.inIncognitoTab
+						}}, function(popup) {
+							hoverZoomWin = popup;
+						});*/
 					}
 				}
 				
