0be8997c549157d2297c0d980d6b374a977a56e4
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2011-03-24 09:15:02 +0000

    Default plugin now excludes file names with a colon (:) (fixes issue 49).

--- plugins/default.js
+++ plugins/default.js
@@ -1,14 +1,14 @@
-// Copyright (c) 2010 Romain Vallet
+// Copyright (c) 2011 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'Default',
-	version: '0.4',
+	version: '0.5',
 	prepareImgLinks: function(callback) {
 		var res = [];
 		$('a[href]').filter(function() {
-			return this.href.match(/\.(?:jpe?g|gif|png|svg|bmp|ico|xbm)(?:[\?#].*)?$/i);
+			return this.href.match(/\/[^:]+\.(?:jpe?g|gif|png|svg|bmp|ico|xbm)(?:[\?#].*)?$/i);
 		}).each(function() {
 			var _this = $(this);
 			if (!_this.data('hoverZoomSrc')) {
