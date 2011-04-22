328643c0a149295e049c3a0f7faba9dd94a092f5
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2011-04-22 16:08:24 +0000

    Added Wordpress, Ipboard and vBulletin plugins. Changed wikipedia plugin to add support for all wikimedia based sites.

---
+++ plugins/wordpress_a.js
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2011 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'Wordpress',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+		if ((hoverZoom.pageGenerator && hoverZoom.pageGenerator.indexOf('WordPress') == -1) || $('img[src*="wp-content"]').length == 0) { return; }
+		var res = [];
+		hoverZoom.urlReplace(res, 
+			'img[src*="wp-content"]',
+			/-\d+x\d+\./,
+			'.'
+		);
+		callback($(res));
+	}
+});
