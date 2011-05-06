fe6e77e11e8ec2eccb48e0582a234ac994e5035f
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2011-05-06 13:29:45 +0000

    Added Youtube plugin.

---
+++ plugins/youtube_a.js
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2011 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'YouTube',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+		var res = [],
+			repl = 'http://i1.ytimg.com/vi/$1/0.jpg';
+		hoverZoom.urlReplace(res, 
+			'img[src*="ytimg.com/vi/"]',
+			/\/(\d|default)\.jpg/,
+			'/0.jpg'
+		);
+		hoverZoom.urlReplace(res, 
+			'a[href*="youtube.com/watch"]',
+			/^.*v=([\w-]+).*$/,
+			repl
+		);
+		hoverZoom.urlReplace(res, 
+			'a[href*="youtu.be/"]',
+			/^.*youtu.be\/([\w-]+).*$/,
+			repl
+		);
+		callback($(res));
+	}
+});
