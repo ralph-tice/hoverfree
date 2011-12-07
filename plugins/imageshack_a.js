a60ee18c110de4de8b7918b1e1fa80390a1430b4
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2011-12-07 17:09:48 +0000

    Fixed imageshack plugin. Tests with twitter api.

--- plugins/imageshack_a.js
+++ plugins/imageshack_a.js
@@ -8,13 +8,18 @@ hoverZoomPlugins.push( {
 	prepareImgLinks: function(callback) {
 		var res = [];
 		hoverZoom.urlReplace(res, 
-			'a[href*="imageshack.us/my.php"], a[href*="imageshack.us/i/"]',
+			'a[href*="imageshack.us/my.php"], a[href*="imageshack.us/i/"',
 			/img(\d+)\.imageshack\.us\/(?:i\/|my\.php\?image=)([\w\.]+).*$/,
 			'desmond.imageshack.us/Himg$1/scaled.php?server=$1&filename=$2&res=medium'
 		);	
 		hoverZoom.urlReplace(res, 
 			'a[href*="imageshack.us/photo/my-images"]',
 			/imageshack\.us\/photo\/my-images\/(\d+)\/([^\/]+)\/$/,
+			'desmond.imageshack.us/Himg$1/scaled.php?server=$1&filename=$2&res=medium'
+		);	
+		hoverZoom.urlReplace(res, 
+			'a[href*="imageshack.us/f/"]',
+			/imageshack\.us\/f\/(\d+)\/([^\/]+)\/$/,
 			'desmond.imageshack.us/Himg$1/scaled.php?server=$1&filename=$2&res=medium'
 		);	
 		callback($(res));
--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 Romain Vallet
+// Copyright (c) 2011 Romain Vallet
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
@@ -11,7 +11,20 @@ hoverZoomPlugins.push( {
 			'img[src*="_mini"]:not([src*="default_profile_"]), img[src*="_normal"]:not([src*="default_profile_"]), img[src*="_bigger"]:not([src*="default_profile_"])',
 			/_(mini|normal|bigger)/,
 			''
-		);	
+		);
+		/*$('a[data-expanded-url*="/photo/"]').one('mouseover', function() {
+			var link = $(this),
+				url = link.attr('data-expanded-url'),
+				id = url.replace(/.*status\/(\d+).*$/, '$1');
+			$.getJSON('http://api.twitter.com/1/statuses/show.json?id=' + id + '&include_entities=true&trim_user=true',
+				function (data) {
+					if (data && data.entities && data.entities.media && data.entities.media.length) {
+						var media = data.entities.media[0];
+						link.data().hoverZoomSrc = (location.protocol == 'https:') ? media.media_url_https : media.media_url;
+					}
+				}
+			);
+		});*/
 		callback($(res));
 	}
 });
