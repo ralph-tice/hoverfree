f796b97c5bf54ca89ba4808f647b616e821dfc21
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2011-12-20 16:08:41 +0000

    Support for twitter photos via API calls.

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -12,19 +12,51 @@ hoverZoomPlugins.push( {
 			/_(mini|normal|bigger)/,
 			''
 		);
-		/*$('a[data-expanded-url*="/photo/"]').one('mouseover', function() {
+		$('a[data-expanded-url]').each(function() {
 			var link = $(this),
-				url = link.attr('data-expanded-url'),
-				id = url.replace(/.*status\/(\d+).*$/, '$1');
-			$.getJSON('http://api.twitter.com/1/statuses/show.json?id=' + id + '&include_entities=true&trim_user=true',
-				function (data) {
-					if (data && data.entities && data.entities.media && data.entities.media.length) {
-						var media = data.entities.media[0];
-						link.data().hoverZoomSrc = (location.protocol == 'https:') ? media.media_url_https : media.media_url;
-					}
-				}
-			);
-		});*/
+				url = this.getAttribute('data-expanded-url');
+			if (url.match(/\/[^:]+\.(?:jpe?g|gif|png|svg|webp|bmp|ico|xbm)(?:[\?#].*)?$/i)) {
+				link.data().hoverZoomSrc = [url];
+				res.push(link);
+			}
+		});
+		$('a[data-expanded-url*="/photo/"]').each(function() {
+			var link = $(this),
+				linkData = link.data(),
+				expandedUrl = link.attr('data-expanded-url'),
+				photoId = expandedUrl.replace(/.*status\/(\d+).*$/, '$1'),
+				storedUrl = localStorage['HZcache_' + photoId];
+			if (storedUrl) {
+				linkData.hoverZoomSrc = [storedUrl];
+				link.addClass('hoverZoomLink');
+			} else {
+				link.mouseenter(function() {
+					linkData.hoverZoomMouseOver = true;
+					if (linkData.hoverZoomTwitterApiCalled) { return; }
+					linkData.hoverZoomTwitterApiCalled = true;
+					$.getJSON('http://api.twitter.com/1/statuses/show.json?id=' + photoId + '&include_entities=true&trim_user=true',
+						function (data) {
+							if (data && data.entities && data.entities.media && data.entities.media.length) {
+								var media = data.entities.media[0],
+									url = (location.protocol == 'https:') ? media.media_url_https : media.media_url;
+									
+								linkData.hoverZoomSrc = [url];
+								link.addClass('hoverZoomLink');
+								
+								// Image is displayed if the cursor is still over the link
+								if (linkData.hoverZoomMouseOver)
+									hoverZoom.displayPicFromElement(link);
+
+								// URLs are stored to lessen API calls
+								localStorage['HZcache_' + photoId] = url;
+							}
+						}
+					);
+				}).mouseleave(function() {
+					linkData.hoverZoomMouseOver = false;
+				});
+			}
+		});
 		callback($(res));
 	}
 });
