5a6514247880b5fb97a490abf37a27c11eeb2728
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2011-12-29 13:10:56 +0000

    Fixed googleusercontent urls (fixes issue 191).

--- plugins/googleplus.js
+++ plugins/googleplus.js
@@ -4,35 +4,19 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'Google+',
-	version: '0.1',
+	version: '0.2',
 	prepareImgLinks: function(callback) {
 		var res = [];
 		hoverZoom.urlReplace(res, 
 			':not([oid]) > img[src*=".googleusercontent.com/-"]',
-			[/\/(w\d+-h\d+|[hws]\d+)(-[npck])*\//, /\?sz=\d+/],
-			['/', '']
+			/\/(w\d+-h\d+|[hws]\d+)(-[npck])*\//,
+			options.showHighRes ? '/s0/' : '/s800/'
 		);		
 		hoverZoom.urlReplace(res, 
 			'img[src*="proxy\?url="]',
 			/.*proxy\?url=([^&]+).*/,
 			'$1'
 		);		
-		// Circles thumbs
-		$('img.a-j-Db-z').each(function() {
-			var _this = $(this),
-				title = _this.next();
-			if (title.length > 0) {
-				_this.data().hoverZoomCaption = title.text();
-			}
-		});
-		// View photos of xxx with...
-		$('.a-b-g-Mp-ba img').each(function() {
-			var _this = $(this),
-				title = _this.parents('.ea-g-Vc').find('a.a-g-h');
-			if (title.length > 0) {
-				_this.data().hoverZoomCaption = title.text();
-			}
-		});
 		callback($(res));	
 	}
 });
