588bec161418b8b9388be439b83ce1e1b7704f05
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-04-18 16:02:09 +0000

    Fixed pinterest plugin.

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,16 +1,26 @@
-// Copyright (c) 2011 Romain Vallet <romain.vallet@gmail.com>
+// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'Pinterest',
-	version: '0.2',
+	version: '0.3',
 	prepareImgLinks: function(callback) {
 		var res = [];
 		hoverZoom.urlReplace(res,
 			'img[src*="_b.jpg"], img[src*="_t.jpg"]',
 			/_[bt]\.jpg/,
 			'_c.jpg'
+		);
+		hoverZoom.urlReplace(res,
+			'img[src*="/avatars/"]',
+			/\.jpg/,
+			'_o.jpg'
+		);
+		hoverZoom.urlReplace(res,
+			'img[src*="type=square"]',
+			'type=square',
+			'type=large'
 		);
 		callback($(res));
 	}
