d630bdceec0d074ffdb80af3e1a0be5ab54ee948
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-04-19 13:28:26 +0000

    Fixed yahoo plugin.

--- plugins/yahoo.js
+++ plugins/yahoo.js
@@ -1,59 +1,30 @@
-// Copyright (c) 2010 Romain Vallet
+// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
+
+// Very similar to google plugin
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'Yahoo',
-	version: '0.3',
+	version: '0.4',
 	prepareImgLinks: function(callback) {
-		var links = $("#yschimg a[href], .sm-media a[href], .imgdd a[href]");
-		var res = [];
-		try {
-			links.each(function() {
-				var _this = $(this);
-				if (_this.attr('href')) {
-					var img = _this.find('img').get(0);
-					var src = _this.attr('href');
-					if (!src) { return; }
-					src = unescape(src);
-					var imgUrlIndex = src.indexOf('imgurl=');
-					if (imgUrlIndex == -1) {
-						if (img) {
-							src = $(img).attr('src');
-							if (src) {
-								imgUrlIndex = src.indexOf('url=');
-								if (imgUrlIndex > -1) {
-									imgUrlIndex -= 3;
-								}
-							}
-						}
-					}
-					if (imgUrlIndex > -1) {
-						src = unescape(src.substring(imgUrlIndex + 7, src.indexOf('&', imgUrlIndex)));		
-						if (src) {
-							if (src.substr(0, 4) != 'http') {
-								src = 'http://' + src;
-							}
-							_this.data().hoverZoomSrc = [src];
-							res.push(_this);
-						}
-					} else {
-						if (img) {
-							src = $(img).attr('src');
-							if (src) {
-								var lastDotIndex = src.lastIndexOf('.');
-								if (src.substr(lastDotIndex - 2, 2) == '-s') {
-									src = unescape(src.replace('-s.', '.'));	
-									_this.data().hoverZoomSrc = [src];
-									res.push(_this);
-								}
-							}
-						}
-					}
-				}
-			});
-		} finally {
-			callback($(res));
+		function prepareImgLink(img) {
+			var img = $(this),
+				link = this.parentNode,
+				href = link.href,
+				imgUrlIndex = href.indexOf('imgurl=');
+			href = href.substring(imgUrlIndex + 7, href.indexOf('&', imgUrlIndex));
+			while (decodeURIComponent(href) != href)
+				href = decodeURIComponent(href);
+			img.data().hoverZoomSrc = [href];
+			img.addClass('hoverZoomLink');
 		}
+		
+		$('a[href*="imgurl="] > img').each(prepareImgLink);
+		$('#ihover-img').load(prepareImgLink);
+		
+        var res = [];
+		hoverZoom.urlReplace(res, 'img[src*="/http"]', /.*\/(http.*)/, '$1');		
+        callback($(res));
 	}
 });
