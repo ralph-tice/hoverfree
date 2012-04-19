67e15e3b39c774c5f8237e0b4f4828e231184627
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-04-19 13:49:51 +0000

    Fixed gawker plugin (fixes issue 222).

--- plugins/gawker.js
+++ plugins/gawker.js
@@ -1,26 +1,27 @@
-﻿// Copyright (c) 2010 Romain Vallet
+﻿// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'Gawker',
-	version: '0.2',
+	version: '0.3',
 	prepareImgLinks: function(callback) {
 		var res = [];
-		$('a img[src*="/assets"]').each(function() {
-			var img = $(this),
-				url = hoverZoom.getThumbUrl(this);
-			if (!url) {	return;	}
-			if (url.indexOf('gizmodo.jp') > -1) {
-				url = url.replace(/^.*\/([^\/]*)-thumb-.*$/, 'http://img.gizmodo.jp/upload_files2/$1.jpg');
-			} else {
-				url = url.replace(/\/(\d+x\d*|gallery|medium|small|xsmall)_/, '/');
-			}
-			if (img.hasClass('avatar')) { url = url.replace(/_\d+\./, '_160.'); }
-			url = url.substr(0, url.lastIndexOf('.'));
-			img.data().hoverZoomSrc = [url + '.jpg', url + '.png', url + '.gif', url];
-			res.push(img);
-		});
+		hoverZoom.urlReplace(res, 
+			'img[src*="img.gawkerassets.com"]',
+			/(gallery|medium|small|xsmall|micro)(_\d+)?/, 
+			'xlarge'
+		);
+		hoverZoom.urlReplace(res, 
+			'img[src*="gizmodo.jp/assets_c"]',
+			/^.*\/([^\/]*)-thumb-.*$/, 
+			'http://img.gizmodo.jp/upload_files2/$1.jpg'
+		);
+		hoverZoom.urlReplace(res, 
+			'img.CommenterImage',
+			/_\d+\./, 
+			'_160.'
+		);
 		hoverZoom.urlReplace(res, 
 			'a img.FB_profile_pic',
 			/_[sqta]\./, 
