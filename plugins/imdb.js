a45ad77a3a62774a6873f3da3f623418caf636bd
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-05-11 16:24:14 +0000

    Fixed IMDb plugin.

--- plugins/imdb.js
+++ plugins/imdb.js
@@ -1,17 +1,24 @@
-// Copyright (c) 2010 Romain Vallet
+// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'IMDb',
-	version: '0.3',
+	version: '0.4',
 	prepareImgLinks: function(callback) {
 		var res = [];
-		hoverZoom.urlReplace(res, 
-			'a img[src*="._V1."]',
-			/\._V1\..*\./,
-			options.showHighRes ? '.' : '._V1._SX600_SY600_.'
-		);
+		$('img[src*="._V1"], img.loadlate, div.rec_poster_img').each(function() {
+			var elem = $(this), url;
+			if (elem.hasClass('loadlate'))
+				url = elem.attr('loadlate');
+			else 
+				url = hoverZoom.getThumbUrl(this);
+			if (url.replace) {
+				url = url.replace(/\._V1.*\./, options.showHighRes ? '.' : '._V1._SX600_SY600_.');
+				elem.data().hoverZoomSrc = [url];
+				res.push(elem);
+			}
+		});
 		callback($(res));
 	}
 });
