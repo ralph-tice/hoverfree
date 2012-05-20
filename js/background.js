38edd2dfb07fd3af0c571705802fb668d49abaf2
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-05-20 21:27:52 +0000

    Moved affiliate links to facebook. Upgraded to version 4.3.1.

--- js/background.js
+++ js/background.js
@@ -144,7 +144,7 @@ function miscStats() {
 function checkUpdate() {
 	var currVersion = chrome.app.getDetails().version,
 		prevVersion = localStorage.hzVersion;
-	if (hasReleaseNotes && options.updateNotifications && currVersion != prevVersion && typeof prevVersion != 'undefined') {
+	if (hasReleaseNotes && prevVersion != '4.3' && options.updateNotifications && currVersion != prevVersion && typeof prevVersion != 'undefined') {
 		webkitNotifications.createHTMLNotification(chrome.extension.getURL('html/update-notif.html')).show();
 	}
 	localStorage.hzVersion = currVersion;
