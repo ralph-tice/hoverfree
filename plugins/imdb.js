6d517b0b9611e6d6a55bdb9cc1d0b5bc5b3b3f1f
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-05-24 12:34:59 +0000

    Fixed IMDb 'loadlate' images (fixes issue 257).

--- plugins/imdb.js
+++ plugins/imdb.js
@@ -4,20 +4,15 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'IMDb',
-	version: '0.4',
+	version: '0.5',
 	prepareImgLinks: function(callback) {
 		var res = [];
 		$('img[src*="._V1"], img.loadlate, div.rec_poster_img').each(function() {
-			var elem = $(this), url;
-			if (elem.hasClass('loadlate'))
-				url = elem.attr('loadlate');
-			else 
-				url = hoverZoom.getThumbUrl(this);
-			if (url.replace) {
-				url = url.replace(/\._V1.*\./, options.showHighRes ? '.' : '._V1._SX600_SY600_.');
-				elem.data().hoverZoomSrc = [url];
-				res.push(elem);
-			}
+			var elem = $(this),
+				url = elem.attr('loadlate') || hoverZoom.getThumbUrl(this);
+			url = url.replace(/\._V1.*\./, options.showHighRes ? '.' : '._V1._SX600_SY600_.');
+			elem.data().hoverZoomSrc = [url];
+			res.push(elem);
 		});
 		callback($(res));
 	}
