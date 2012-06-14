69a4c6961e24484bff8e11c2c489f2634f10b779
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-06-14 15:53:21 +0000

    Fixed ebay plugin (fixes issue 242). Updated to version 4.4.

--- plugins/ebay.js
+++ plugins/ebay.js
@@ -1,12 +1,12 @@
-// Copyright (c) 2010 Romain Vallet
+// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
-// This one is quite different from the others as it makes use of the eBay API
+// This plugin makes use of the eBay API
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'eBay',
-	version: '0.2',
+	version: '0.3',
 	prepareImgLinks: function(callback) {
 		var res = [];
 		var appId = 'RomainVa-3007-4951-b943-aaedf0d9af84';
@@ -14,34 +14,49 @@ hoverZoomPlugins.push( {
 		var itemIndex = 0;
 		var hzItems = [], itemIds = [];
 		var cachePrefix = 'cache_eBayItem_' + (options.showHighRes ? 'hi' : 'lo') + '_';
+
+		hoverZoom.urlReplace(res, 
+			'img[src*="~~"]',
+			/~~(\d+)?_\d+\./,
+			'~~$1_32.'
+		);
+		
+		function getIdFromURL(url) {
+			if (!url) { return false; }
+			var reg = url.match(/\d{9,12}/);
+			if (!reg) { return false; }
+			return reg[0];
+		}
 	
 		// First we gather all the products on the page and we store their eBay ID and 
 		// the link that will receive the 'hoverZoomSrc' data.
-		$('a.l-tn, table.l-tn a, td.gv-gvtd a, a.pic, table.pic a, a.gpvi, a.img').each(function() {
-			var item = {link: this, id: ''};
-			var parent = $(this).parents('table:eq(0)');
-			if (parent.hasClass('l-tn') || parent.hasClass('gv-gvimg') || parent.hasClass('pic')) {
-				parent = parent.parents('div:eq(0)');
+		$('a img[src*="ebaystatic.com"]').each(function() {
+			var img = $(this);
+			if (img.hasClass('imgPrv') || img.parents('.hol').length) { return; }
+			var item = { thumb: this, id: '' },
+				link = img.parents('a[href*=".ebay."]:eq(0)');
+			if (!link.length) {
+				link = $(this).parents('table:eq(0)').find('a[href*=".ebay."]');
 			}
-			var dtlLink = parent.find('div.l-its a, div.gv-gvitmttl a, div.ittl a, div.ttl a');
-			if (dtlLink.length > 0) {
-				item.id = dtlLink.attr('href');
-				item.id = item.id.substr(item.id.lastIndexOf('/') + 1);
-				item.id = item.id.substr(0, item.id.indexOf('?'));
+			if (!link.length) {
+				link = $(this).parents('div:eq(0)').find('a[href*=".ebay."]');
+			}
+			item.id = getIdFromURL(link.attr('href'));
+			if (item.id) {
 				itemIds.push(item.id);
 				hzItems.push(item);
 			}
-		});		
+		});	
 		
 		// Check if some urls were stored
 		for (var i = 0; i < hzItems.length; i++) {
 			var storedItem = localStorage[cachePrefix + hzItems[i].id];
 			if (storedItem) {
 				storedItem = JSON.parse(storedItem);
-				var link = $(hzItems[i].link), data = link.data();
+				var thumb = $(hzItems[i].thumb), data = thumb.data();
 				data.hoverZoomSrc = [storedItem.pictureUrl];
 				data.hoverZoomCaption = storedItem.title;
-				res.push(link);
+				res.push(thumb);
 				hzItems.splice(i, 1);
 				i--;
 			}
@@ -60,21 +75,27 @@ hoverZoomPlugins.push( {
 			var itemBunch = itemIds.slice(itemIndex, indexEnd);
 			itemIndex = indexEnd;			
 			var requestUrl = requestUrlBase + itemBunch.join(',');
+			//console.log('requestUrl: ' + requestUrl);
 			
 			// Ajax calls are made through the background page (not possible from a content script)
 			chrome.extension.sendRequest({action: 'ajaxGet', url: requestUrl}, function(data) {
+				//console.log(data);
 				var getMultipleItemsResponse = JSON.parse(data);
+				if (getMultipleItemsResponse.Errors)
+					return;
 				for (var i=0; i<getMultipleItemsResponse.Item.length; i++) {
 					var item = getMultipleItemsResponse.Item[i];
+							//console.log(item);
 					for (var j=0; j<hzItems.length; j++) {
 						if (hzItems[j].id == item.ItemID && item.PictureURL && item.PictureURL.length > 0) {
-							var link = $(hzItems[j].link), data = link.data();
-							data.hoverZoomSrc = [item.PictureURL[0]];
+							var thumb = $(hzItems[j].thumb), data = thumb.data(), url = item.PictureURL[0].replace(/~~(\d+)?_\d+\./, '~~$1_32.');
+							data.hoverZoomSrc = [url];
 							data.hoverZoomCaption = item.Title;
-							res.push(link);
+							//console.log(thumb);
+							res.push(thumb);
 							
 							// Items are stored to lessen API calls
-							localStorage[cachePrefix + item.ItemID] = JSON.stringify({pictureUrl: item.PictureURL[0], title: item.Title});
+							localStorage[cachePrefix + item.ItemID] = JSON.stringify({pictureUrl: url, title: item.Title});
 						}
 					}
 				}
