fe144f4d620cd0820f9ffb1c8e1523906cb3a9f7
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-06-26 16:11:32 +0000

    Added external deviantArt links support (fixes issue 248).

---
+++ plugins/deviantart_a.js
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'deviantART',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+		$('a[href*=".deviantart.com/art/"], a[href^="http://fav.me/"]').one('mouseenter', function() {
+			hoverZoom.prepareOEmbedLink(this, 'http://backend.deviantart.com/oembed?url=', this.href);
+		});
+	}
+});
