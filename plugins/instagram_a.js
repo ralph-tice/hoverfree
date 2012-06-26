ebca2584d9e6e93f03e12a24384500844323fcce
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-06-26 15:39:25 +0000

    Added Instagram plugin (fixes issue 193).

---
+++ plugins/instagram_a.js
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'Instagram',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+		var res = [],
+			search = /.*instagr\.am\/p\/([^\/]+).*/,
+			replace = 'http://instagr.am/p/$1/media/?size=l';
+		hoverZoom.urlReplace(res, 'a[ref*="instagr.am/p/"]', search, replace);
+		$('a[data-expanded-url*="instagr.am/p/"]').each(function() {
+			var link = $(this);
+			link.data().hoverZoomSrc = [this.dataset['expandedUrl'].replace(search, replace)];
+			res.push(link);
+		});
+		if (res.length) { callback($(res)); }
+	}
+});
