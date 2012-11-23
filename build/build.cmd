bb7f3540ee420fdf3c194399b1e9f09a2d97e934
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-11-23 16:55:43 +0000

    Updated builder to 4.7.3. Builder now deletes destination files if they exist.

--- build/build.cmd
+++ build/build.cmd
@@ -2,13 +2,13 @@ rem @echo off
 
 setlocal
 
-set HZ_VERSION=4.7.1
+set HZ_VERSION=4.7.3
 set SRC_DIR=..\src
 set DEST_DIR=%~dp0releases\hoverzoom_%HZ_VERSION%
 
-
 java -jar tools\compiler.jar --js "%SRC_DIR%\js\affiliate.js" --js_output_file "%SRC_DIR%\js\affiliate.min.js"
 
+del /s /q "%DEST_DIR%"
 md "%DEST_DIR%"
 xcopy /s /y "%SRC_DIR%\*.*" "%DEST_DIR%\*.*"
 del "%DEST_DIR%\js\affiliate.js"
