a3615ccf698ea218e8900375f03aca060ff1c024
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-12-11 17:24:58 +0000

    Added Newegg plugin (fixes issue 381).

---
+++ src/plugins/newegg.js
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'NewEgg',
+	prepareImgLinks: function(callback) {
+		var res = [];
+		hoverZoom.urlReplace(res, 
+			'img[src*="?$"]',
+			/\?.*$/,
+			''
+		);
+		hoverZoom.urlReplace(res, 
+			'img[src*="ProductImageCompress"]',
+			/NeweggImage\/ProductImageCompress.*\//,
+			'productimage/'
+		);
+		callback($(res));
+	}
+});
