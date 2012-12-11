919cec0b062482fe1408a73580d73a37f2042331
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-12-11 17:08:21 +0000

    Fixed deviantArt (fixes issue 392)

--- src/plugins/deviantart.js
+++ src/plugins/deviantart.js
@@ -4,39 +4,13 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
 	name: 'deviantART',
-	version: '0.6',
 	prepareImgLinks: function(callback) {
 		var res = [];
-		
-		/*var	https = (document.location.protocol == 'https:'),
-			filter = https ? 'a img[src*="/th/"]' : 'a img[src^="http://th"]',
-			sizeIndex = https ? 5 : 4;
-		$(filter).each(function() {
-			var img = $(this);
-			var src = img.attr('src'), srcLo;
-			if (!src) { return; }
-			var aSrc = src.split('/');
-			if (!options.showHighRes) {
-				aSrc[sizeIndex] = 'PRE';
-				srcLo = aSrc.join('/');
-			}
-			aSrc.splice(sizeIndex, 1);
-			src = aSrc.join('/');
-			// deviantArt tends to refuse to load images sometimes.
-			// src is stored several times in hoverZoomSrc so that it may retry several times if loading fails.
-			srcs = [src + '#1', src + '#2', src + '#3', src + '#4'];
-			if (!options.showHighRes) {
-				srcs = [srcLo + '#1', srcLo + '#2'].concat(srcs);
-			}
-			var link = img.parents('a:eq(0)');
-			link.data().hoverZoomSrc = srcs;
-			res.push(link);
-		});*/
-		$('a[super_img]').each(function() {
+		$('a[data-super-img]').each(function() {
 			var _this = $(this),
-				url = this.getAttribute('super_img');
-			if (options.showHighRes && this.hasAttribute('super_fullimg'))
-				url = this.getAttribute('super_fullimg');
+				url = this.dataset.superImg;
+			if (options.showHighRes && this.dataset.superFullImg)
+				url = this.dataset.superFullImg;
 			_this.data().hoverZoomSrc = [url];
 			res.push(_this);
 		});
