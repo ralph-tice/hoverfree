167e6d78be9dfa5a52501ae669fdcc371abaa361
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-12-20 17:15:31 +0000

    Fixed zip command line.

--- build/build.cmd
+++ build/build.cmd
@@ -2,7 +2,7 @@ rem @echo off
 
 setlocal
 
-set HZ_VERSION=4.7.3
+set HZ_VERSION=4.8
 set SRC_DIR=..\src
 set DEST_DIR=%~dp0releases\hoverzoom_%HZ_VERSION%
 
@@ -13,7 +13,7 @@ md "%DEST_DIR%"
 xcopy /s /y "%SRC_DIR%\*.*" "%DEST_DIR%\*.*"
 del "%DEST_DIR%\js\affiliate.js"
 
-tools\7za.exe a -r -tzip "releases\hoverzoom_%HZ_VERSION%.zip" "%SRC_DIR%\*.*"
+tools\7za.exe a -r -tzip "releases\hoverzoom_%HZ_VERSION%.zip" "%DEST_DIR%\*.*"
 
 %LOCALAPPDATA%\Google\Chrome\Application\chrome.exe --pack-extension="%DEST_DIR%" --pack-extension-key="%~dp0hoverzoom.pem" --no-message-box
 
