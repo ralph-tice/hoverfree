990d3cc5ffb67d6b69b902ff6e6d0afdecd16829
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2012-12-24 10:26:12 +0000

    Fixed deviantArt links with no data-super-img attribute. Fixes issue 404. Fixes issue 412.

--- src/plugins/deviantart.js
+++ src/plugins/deviantart.js
@@ -14,6 +14,12 @@ hoverZoomPlugins.push( {
 			_this.data().hoverZoomSrc = [url];
 			res.push(_this);
 		});
+		hoverZoom.urlReplace(res, 
+			'a:not([data-super-img]) img[src*="deviantart.net/fs"]',
+			/\/(fs\d+)\/\d+\w+\//,
+			'/$1/',
+			':eq(0)'
+		);		
 		callback($(res));
 	}
 });
