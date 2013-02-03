4759c3d1593bd22afc4272e7b87005b58b69961c
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2013-02-03 10:37:30 +0000

    Fixed pinterest plugin (fixes issue 457).

--- src/plugins/pinterest.js
+++ src/plugins/pinterest.js
@@ -1,26 +1,27 @@
-// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
+// Copyright (c) 2013 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Pinterest',
-    version:'0.3',
     prepareImgLinks:function (callback) {
         var res = [];
-        hoverZoom.urlReplace(res,
-            'img[src*="_b.jpg"], img[src*="_t.jpg"]',
-            /_[bt]\.jpg/,
-            '_c.jpg'
-        );
+		$('div.pin').each(function(){
+			var _this = $(this),
+				img = _this.find('img.PinImageImg'),
+				url = this.dataset ? this.dataset.closeupUrl : false;
+			if (img.length) {
+				if (!url) {
+					url = img.attr('src').replace('/192/', '/550/')
+				}
+				img.data().hoverZoomSrc = [url];
+				res.push(img);
+			}
+		});
         hoverZoom.urlReplace(res,
             'img[src*="/avatars/"]',
             /\.jpg/,
             '_o.jpg'
-        );
-        hoverZoom.urlReplace(res,
-            'img[src*="type=square"]',
-            'type=square',
-            'type=large'
         );
         callback($(res));
     }
