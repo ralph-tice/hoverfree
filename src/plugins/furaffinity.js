d375f4b7bb56e4502df2c3daa6893d2b8f9e54cf
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2013-02-05 15:44:35 +0000

    Added FurAffinity plugin (fixes issue 444).

---
+++ src/plugins/furaffinity.js
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2013 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'FurAffinity',
+    prepareImgLinks:function (callback) {
+        var res = [];
+        $('a[href*="/view/"]').one('mouseover', function() {
+            hoverZoom.prepareFromDocument($(this), this.href, function(doc) {
+                var srcLink = doc.querySelector('a[href*="d.facdn.net"]');
+                return srcLink ? srcLink.href : false;
+            });
+        });
+        callback($(res));
+    }
+});
