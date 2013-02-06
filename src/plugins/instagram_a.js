7b8b961f6e35cbc11334fbd3616f4dfe5d40a24c
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2013-02-06 17:08:35 +0000

    Fixed Instagram plugin (fixes issue 462).

--- src/plugins/instagram_a.js
+++ src/plugins/instagram_a.js
@@ -1,15 +1,14 @@
-﻿// Copyright (c) 2012 Romain Vallet <romain.vallet@gmail.com>
+﻿// Copyright (c) 2013 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Instagram',
-    version:'0.1',
     prepareImgLinks:function (callback) {
         var res = [],
             search = /.*(?:instagr\.am|instagram\.com)\/p\/([^\/]+).*/,
             replace = 'http://instagr.am/p/$1/media/?size=l';
-        hoverZoom.urlReplace(res, 'a[ref*="instagr.am/p/"], a[ref*="instagram.com/p/"]', search, replace);
+        hoverZoom.urlReplace(res, 'a[href*="instagr.am/p/"], a[href*="instagram.com/p/"]', search, replace);
         $('a[data-expanded-url*="instagr.am/p/"], a[data-expanded-url*="instagram.com/p/"]').each(function () {
             var link = $(this);
             link.data().hoverZoomSrc = [this.dataset['expandedUrl'].replace(search, replace)];
