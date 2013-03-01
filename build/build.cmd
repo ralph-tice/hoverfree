9192a6b585783e14e6083e3ee9e7e935380737f6
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2013-03-01 13:27:52 +0000

    Updated to 4.11.3. Added stats script.

--- build/build.cmd
+++ build/build.cmd
@@ -2,7 +2,7 @@ rem @echo off
 
 setlocal
 
-set HZ_VERSION=4.11.1
+set HZ_VERSION=4.11.3
 set SRC_DIR=..\src
 set DEST_DIR=%~dp0releases\hoverzoom_%HZ_VERSION%
 
@@ -12,6 +12,7 @@ del /s /q "%DEST_DIR%"
 md "%DEST_DIR%"
 xcopy /s /y "%SRC_DIR%\*.*" "%DEST_DIR%\*.*"
 del "%DEST_DIR%\js\affiliate.js"
+del "%DEST_DIR%\js\amstats.js"
 
 tools\7za.exe a -r -tzip "releases\hoverzoom_%HZ_VERSION%.zip" "%DEST_DIR%\*.*"
 
